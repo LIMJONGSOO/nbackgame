15d71de5bd75979a8d7ea52fd28add0d3f977dde
jslim <ec.jslim@kalmate.net>
2020-09-21 17:20:07 +0900

    Updates

---
+++ precache-manifest.596b7cec42c121970e9125d1dbb69622.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5377b0905ec5a8f26f1dd714a323431",
+    "url": "/nbackgame/index.html"
+  },
+  {
+    "revision": "a5dd3735f3652928da1b",
+    "url": "/nbackgame/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91d54a0024c710d8a6e2",
+    "url": "/nbackgame/static/js/2.9f1d4771.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nbackgame/static/js/2.9f1d4771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5dd3735f3652928da1b",
+    "url": "/nbackgame/static/js/main.bca5bf42.chunk.js"
+  },
+  {
+    "revision": "43be5f63b5cd807a675f",
+    "url": "/nbackgame/static/js/runtime-main.a4316ea6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nbackgame/static/media/logo.5d5d9eef.svg"
+  }
+]);
